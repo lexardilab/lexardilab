7ab92d5444476bbc1f564b5999e48ebd36d484cc
lexardi <lexardi.lab@gmail.com>
2024-04-17 14:05:47 +0200

    commit

---
+++ src/app/cart/layout.jsx
@@ -0,0 +1,21 @@
+import { Inter } from "next/font/google";
+import "../globals.css";
+import Navbar from "../components/Navbar";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "Lexardi Lab",
+  description: "Lexardi is laboratory of ideas",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <Navbar />
+        {children}
+      </body>
+    </html>
+  );
+}
