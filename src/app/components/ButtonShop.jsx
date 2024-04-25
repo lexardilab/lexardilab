3183f236bc9258aecb92899fff41cdbdce3662bc
lexardi <lexardi.lab@gmail.com>
2024-04-25 16:06:53 +0200

    button

--- src/app/components/ButtonShop.jsx
+++ src/app/components/ButtonShop.jsx
@@ -1,13 +1,52 @@
+import Image from "next/image";
 import Link from "next/link";
 
 export default function ButtonShop() {
   return (
-    <div className="flex justify-end px-6">
-      <button className="px-4 py-2 font-semibold text-black bg-transparent border border-black hover:bg-black hover:text-white hover:border-transparent">
-        <Link href="https://buy.stripe.com/fZe17T9bK8Qp50caEE" target="_blank">
-          Comprar
-        </Link>
-      </button>
-    </div>
+    <>
+      <div className="flex justify-center px-6 py-12">
+        <button className="px-4 py-2 font-semibold text-black bg-transparent border border-black hover:bg-black hover:text-white hover:border-transparent">
+          <Link
+            href="https://buy.stripe.com/fZe17T9bK8Qp50caEE"
+            target="_blank"
+          >
+            Comprar
+          </Link>
+        </button>
+      </div>
+      <div className="flex items-center justify-center">
+        <div className="grid grid-cols-4 ">
+          <div className="flex items-center justify-center ">
+            <Image
+              src="/certificados/Certificado_Gots.svg"
+              width="70"
+              height="70"
+            />
+          </div>
+          <div className="flex items-center justify-center ">
+            <Image
+              src="/certificados/Certificado_OekoTex.svg"
+              width="70"
+              height="70"
+            />
+          </div>
+          <div className="flex items-center justify-center ">
+            <Image
+              src="/certificados/Certificado_Peta.svg"
+              width="170"
+              height="70"
+            />
+          </div>
+
+          <div className="flex items-center justify-center pl-12 ">
+            <Image
+              src="/certificados/Certificado_Wear_Fair.svg"
+              width="170"
+              height="70"
+            />
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -4,11 +4,13 @@ import Link from "next/link";
 
 export default function Navbar() {
   return (
-    <div className="flex items-center justify-between px-6 py-6 border-b-2 border-black">
-      <Link href="/">
-        <Image src="/borobil_yellow.svg" width="40" height="60" />
-      </Link>
-      <div className="flex ">
+    <div className="grid grid-cols-3 px-6 py-6 border-b-2 border-black">
+      <div>
+        <Link href="/">
+          <Image src="/borobil_yellow.svg" width="40" height="60" />
+        </Link>
+      </div>
+      <div className="flex items-center justify-center">
         <Link href="/tienda">
           <h1
             className={`${montserrat.className} px-4 font-bold text-xl hover:underline cursor-pointer`}
@@ -29,7 +31,7 @@ export default function Navbar() {
           </h1>
         </Link>
       </div>
-      <div className="flex">
+      <div className="flex items-center justify-end">
         <h1 className={`${roboto_mono.className} px-2  text-sm cursor-pointer`}>
           Newsletter
         </h1>
